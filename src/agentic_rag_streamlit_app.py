791f7b38bd2f455f41fc0aba4f466bdb3d816cdf
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-06-18 16:05:56 +0800

    feat: Integrate Streamlit-based UI for Agentic RAG chatbot.

---
+++ src/agentic_rag_streamlit_app.py
@@ -0,0 +1,133 @@
+# import basics
+import os
+from dotenv import load_dotenv
+
+# import streamlit
+import streamlit as st
+
+# import langchain
+from langchain.agents import AgentExecutor
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
+from langchain.agents import create_tool_calling_agent
+from langchain import hub
+from langchain_core.prompts import PromptTemplate
+from langchain_community.vectorstores import SupabaseVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.tools import tool
+
+# import supabase db
+from supabase.client import Client, create_client
+
+# import utils
+import src.utils.config_loader as config_loader
+
+# load environment variables
+load_dotenv()  
+
+config_file_path = os.environ.get("CONFIG_FILE_PATH")
+config = config_loader.load_config(config_file_path)
+
+# initiating supabase
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
+supabase: Client = create_client(supabase_url, supabase_key)
+
+# initiating embeddings model
+embeddings = OpenAIEmbeddings(model=config["embedding"]["model"])
+
+table_name=config["database"]["supabase"]["table"]
+query_name=config["database"]["supabase"]["query_function"]
+
+# initiating vector store
+vector_store = SupabaseVectorStore(
+    embedding=embeddings,
+    client=supabase,
+    table_name=table_name,
+    query_name=query_name,
+)
+ 
+# initiating llm
+llm = ChatOpenAI(model=config["llm"]["model"],temperature=config["llm"]["temperature"])
+
+# Convert YAML prompt config to LangChain message templates
+messages = []
+for entry in config["prompt"]:
+    if entry["type"] == "placeholder":
+        messages.append(
+            MessagesPlaceholder(
+                variable_name=entry["variable_name"],
+                optional=entry.get("optional", False)
+            )
+        )
+    else:
+        messages.append((entry["type"], entry["content"]))
+
+# Build ChatPromptTemplate
+prompt = ChatPromptTemplate.from_messages(messages)
+
+
+# creating the retriever tool
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+# combining all tools
+tools = [retrieve]
+
+# initiating the agent
+agent = create_tool_calling_agent(llm, tools, prompt)
+
+# create the agent executor
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+# initiating streamlit app
+st.set_page_config(page_title="Agentic RAG Chatbot", page_icon="🦜")
+st.title("🦜 Agentic RAG Chatbot")
+
+# initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# display chat messages from history on app rerun
+for message in st.session_state.messages:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("user"):
+            st.markdown(message.content)
+    elif isinstance(message, AIMessage):
+        with st.chat_message("assistant"):
+            st.markdown(message.content)
+
+
+# create the bar where we can type messages
+user_question = st.chat_input("How are you?")
+
+
+# did the user submit a prompt?
+if user_question:
+
+    # add the message from the user (prompt) to the screen with streamlit
+    with st.chat_message("user"):
+        st.markdown(user_question)
+
+        st.session_state.messages.append(HumanMessage(user_question))
+
+
+    # invoking the agent
+    result = agent_executor.invoke({"input": user_question, "chat_history":st.session_state.messages})
+
+    ai_message = result["output"]
+
+    # adding the response from the llm to the screen (and chat)
+    with st.chat_message("assistant"):
+        st.markdown(ai_message)
+
+        st.session_state.messages.append(AIMessage(ai_message))
